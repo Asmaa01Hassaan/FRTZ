09f68a837e47d0255d911b16a04dba0579a7142f
Asmaa01Hassaan <asmaa12.elsayed01@gmail.com>
2025-09-23 14:13:48 +0300

    add installment to order line

--- account_invoice_installments/models/product_template.py
+++ account_invoice_installments/models/product_template.py
@@ -1,5 +1,5 @@
 # models/product_template.py
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -11,3 +11,17 @@ class ProductTemplate(models.Model):
     installment_number = fields.Integer(string="No. of Installments")
     first_payment = fields.Float(string="First Payment")
     installment_amount = fields.Float(string="Installment Amount")
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    # mirror the product’s flag to control visibility in the view
+    installments_allowed = fields.Boolean(
+        related='product_id.product_tmpl_id.installments_allowed',
+        store=False
+    )
+
+    installment_number = fields.Integer(string="No. of Installments")
+    first_payment = fields.Float(string="First Payment")
+    installment_amount = fields.Float(string="Installment Amount")
+
