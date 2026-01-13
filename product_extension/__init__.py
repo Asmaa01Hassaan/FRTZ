1068eacc75ba764ad1d3883cf949c2f8b4e43deb
Asmaa01Hassaan <asmaa12.elsayed01@gmail.com>
2026-01-13 05:05:10 +0200

    add display type text to attribute

--- product_extension/__init__.py
+++ product_extension/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 from . import models
 
+def post_init_hook(cr, registry):
+    """Migrate existing records with display_type='text' to 'pills'"""
+    cr.execute("""
+        UPDATE product_attribute
+        SET display_type = 'pills',
+            allow_free_text = true,
+            create_variant = 'no_variant'
+        WHERE display_type = 'text'
+    """)
+
--- product_extension/__manifest__.py
+++ product_extension/__manifest__.py
@@ -22,5 +22,6 @@
     "application": False,
     "installable": True,
     "auto_install": False,
+    "post_init_hook": "post_init_hook",
 }
 
